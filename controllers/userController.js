9981e4fa7ccc2feadbf94859224a1bf00521b5d9
Zhan Tong <zhantong94@gmail.com>
2023-04-05 09:06:44 +0800

    Update & delete function added to userRoutes and controllerRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,29 @@ const createUsers = (data, res) => {
       throw err;
     });
 };
+
+const updateUsers = (req, res) => {
+  Models.User.update(req.body, { where: { id: req.params.id } })
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+const deleteUsers = (req, res) => {
+  Models.User.destroy({ where: { id: req.params.id } })
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
 module.exports = {
   getUsers,
   createUsers,
+  updateUsers,
+  deleteUsers,
 };
