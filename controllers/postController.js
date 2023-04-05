9d063b85bf9a75b1545060b808956390858e1290
Zhan Tong <zhantong94@gmail.com>
2023-04-05 09:48:52 +0800

    added Post to routes and controllers folder

---
+++ controllers/postController.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const Models = require("../models");
+
+const getPost = (res) => {
+  Models.Post.findAll({})
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const createPost = (data, res) => {
+  Models.Post.create(data)
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+module.exports = {
+  getPost,
+  createPost,
+};
