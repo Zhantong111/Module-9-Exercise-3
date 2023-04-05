36afba18377818ffacd6d5d6f42665191f5adc71
Zhan Tong <zhantong94@gmail.com>
2023-04-05 10:59:35 +0800

    Add comment to comment routes and controllers

---
+++ controllers/commentController.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const Models = require("../models");
+const { sequelize } = require("../models/user");
+
+const getComment = (res) => {
+  Models.Comment.findAll({})
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const createComment = (data, res) => {
+  Models.Comment.create(data)
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const updateComment = (req, res) => {
+  Models.Comment.update(req.body, { where: { id: req.params.id } })
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+const deleteComment = (req, res) => {
+  Models.Comment.destroy({ where: { id: req.params.id } })
+    .then(function (data) {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+module.exports = {
+  getComment,
+  createComment,
+  updateComment,
+  deleteComment,
+};
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use("/api/users", userRoutes);
 let postRoutes = require("./routes/postRoutes");
 app.use("/api/posts", postRoutes);
 
+let commentRoutes = require("./routes/commentRoutes");
+app.use("/api/comments", commentRoutes);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my SQL application." });
 });
